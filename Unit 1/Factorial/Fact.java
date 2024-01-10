156ead36f930f06b32e6273a1e139840fc01984c
Omprakash Rawat <omprakash824rawat@gmail.com>
2024-01-10 16:24:23 +0530

    Factorial

---
+++ Unit 1/Factorial/Fact.java
@@ -0,0 +1,16 @@
+class Fact
+{
+    static int fact(int n)
+    {
+        if (n <= 1)
+        {
+            return 1;
+        }
+        return fact(n-1)*n;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(fact(5));
+    }
+}
